13531d76cf30e21f01918a450079d967da9d748e
higuysdorobloxjoaopk-maker <higuysdorobloxjoaopk@gmail.com>
2026-03-08 19:44:40 -0300

    Update script.lua

--- data/scripts/exemple/script.lua
+++ data/scripts/exemple/script.lua
@@ -1,1 +1,14 @@
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
 
+print("AutoFarm iniciado")
+
+while true do
+    task.wait(1)
+
+    for _,coin in pairs(workspace:GetDescendants()) do
+        if coin.Name == "Coin" then
+            char:MoveTo(coin.Position)
+        end
+    end
+end
